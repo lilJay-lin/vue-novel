aeb449d7692236e144e7fec062e7e2c9c0c08cbf
lilJay-lin <lin_xjie@foxmail.com>
2016-11-22 15:08:59 +0800

    引入materialize最为基础样式

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App'
 import store from './vuex/store'
 
+require('./assets/scss/materialize.config.scss')
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
