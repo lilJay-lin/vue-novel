62a29d48a052854c664b55bac87fe2bd40ca3d31
lilJay-lin <lin_xjie@foxmail.com>
2016-11-26 18:02:10 +0800

    loading增加开始加载标识

--- src/mixins/loading.js
+++ src/mixins/loading.js
@@ -4,13 +4,25 @@
 import Loading from '../components/Loading'
 import {mapActions} from 'vuex'
 export default {
+  data () {
+    return {
+      disabledLoading: false
+    }
+  },
   components: {
     Loading
   },
   methods: {
     ...mapActions(['startLoading', 'loadingError', 'loadingSuccess']),
+    beforeFetch () {
+      return true
+    },
     startFetch () {
       let vm = this
+      if (vm.disabledLoading === true) {
+        return
+      }
+      vm.beforeFetch()
       vm.startLoading()
       vm.fetchData().then(() => {
         vm.loadingSuccess()
